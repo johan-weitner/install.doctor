c81596e016c508910ef339898dea0babfb80c75e
johanweitner@gmail.com <johanweitner@gmail.com>
2024-06-13 19:53:52 +0200

    WiP

--- preparemac.sh
+++ preparemac.sh
@@ -2,11 +2,13 @@
 echo "Preparing MacOS system for Install Doctor..."
 
 export HEADLESS_INSTALL=true
-export SOFTWARE_GROUP=Standard-Desktop
+export SOFTWARE_GROUP="Standard-Desktop"
 export FULL_NAME="Johan Weitner"
 export PRIMARY_EMAIL="johanweitner@gmail.com"
-export PUBLIC_SERVICES_DOMAIN="johan.weitner"
-export START_REPO=johan-weitner/install.doctor
-export START_BRANCH=main
+export PUBLIC_SERVICES_DOMAIN="mbp2018.johan.weitner"
+export START_REPO="johan-weitner/install.doctor"
+export START_BRANCH="main"
 export WORK_ENVIRONMENT=false
-export SOFTWARE_GROUP=""
+export SOFTWARE_GROUP="Standard-Desktop"
+
+bash <(curl -sSL https://install.doctor/start)
